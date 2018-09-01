f6154cf76e29754c9092aa4db2cf51f44bcfa0e9
Phill Barron <phillipbarron242@gmail.com>
2018-09-01 21:13:27 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Express sandbox
+
+test-bed for CORs setup from express
---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+app.use(cors());
+app.get('/', (req, res) => res.send('HEADERS'));
+
+app.listen(3000, () => console.log('what?'));
